8bbae4a3e807940378f70658ee1f3684e6eba803
KimSnowFlower <kimbona1148@gmail.com>
2025-02-22 10:37:05 +0900

    test2

--- src/config/openAi.js
+++ src/config/openAi.js
@@ -1,4 +1,4 @@
-import { config } from 'dotenv';
-config();
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const OPEN_API_KEY = process.env.OPEN_API_KEY || 'default_key';
--- src/config/twilio.js
+++ src/config/twilio.js
@@ -1,5 +1,5 @@
-import { config } from 'dotenv';
-config();
+import dotenv from 'dotenv';
+dotenv.config();
 import twilio from 'twilio';
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID || 'default-sid';
